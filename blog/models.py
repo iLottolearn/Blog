c2e075545e441a71594db4f3b6017cdfa5330647
L <minesteam@outlook.com>
2018-10-21 22:31:40 +0800

    添加gitalk

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db.models.fields import exceptions
+from django.utils import timezone
 from read_statistics.models import ReadNumExpandMethod
 # Create your models here.
 
@@ -19,7 +20,7 @@ class Blog(models.Model,ReadNumExpandMethod):
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)
     content = RichTextUploadingField()
     author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    created_time = models.DateTimeField(auto_now_add=True)
+    created_time = models.DateTimeField(default=timezone.now)
     last_updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
